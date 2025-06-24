bd80b98a6b09fce3770c5005d771143ba2782887
JhiteshGuptha <jhiteshguptha.1785@gmail.com>
2025-06-24 10:37:17 -0400

    Added Output File for Reference

---
+++ Output.py
@@ -0,0 +1,17 @@
+# is a comment, not displayed in the output
+
+# Each python file is saved using .py extension
+
+# Output in Python
+
+# Python outputs each statement or line of code in a new line by default
+
+# We can output both strings and integers using the same print statement
+
+print("Welcome to Code.io")
+print("We are learning Python")
+print(1) # integer output
+print("1") # string output 
+
+
+
