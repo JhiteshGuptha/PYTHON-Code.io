d3ec2413915ee195c9f7b7f1c9257af9423c9493
JhiteshGuptha <jhiteshguptha.1785@gmail.com>
2025-06-24 10:41:36 -0400

    Added Variables File for Reference

---
+++ Variables.py
@@ -0,0 +1,34 @@
+# Variable = container for a value (string, integer, float, boolean)
+#            The value in a variable can be changed at any moment unlike constants
+
+# Declaring variable and assigning value
+
+# Strings
+
+name = "Code.io"
+learning = "Python"
+
+# Integer
+
+year = 2025
+quantity = 3
+
+# Float
+
+price = 10.99
+height = 175.4
+
+# Boolean [True or False]
+#Boolean are mostly used with conditional statements
+
+isActive = True
+
+# Printing variable with a text using F string {Placeholder}
+
+print(f"Hello {name}")
+print(f"We are learning {learning}")
+print(f"Current Year: {year}")
+print(f"Quantity: {quantity}")
+print(f"Price is ${price}")
+print(f"Height: {height}cm")
+print(f"Is the user active: {isActive}")
