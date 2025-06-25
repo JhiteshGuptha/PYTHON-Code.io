4796e0ba5a83be55d9147f6296af37e20dc842ad
JhiteshGuptha <jhiteshguptha.1785@gmail.com>
2025-06-25 12:37:41 -0400

    Added MathFunction File for Reference

---
+++ MathFunction.py
@@ -0,0 +1,46 @@
+import math
+
+# Math Functions to perform Math Operations
+
+p = 3.14156
+q = 4
+r = 5
+
+# Round Function to the value mentioned
+
+roundP = round(p, 2)
+print(roundP)
+
+# Power Function [x to the power y]
+
+power = pow(4, 5)
+print(power)
+
+# Max Function
+
+maximum = max(p, q, r)
+print(maximum)
+
+# Min Function
+
+minimum = min(p, q, r)
+print(minimum)
+
+# Value of PI using Math Module
+
+print(math.pi)
+
+# Square Root of a number
+
+squareRoot = math.sqrt(q)
+print(squareRoot)
+
+# Ceil Function: Rounds a floating point number upwards
+
+roundUp = math.ceil(p)
+print(roundUp)
+
+# Floor Function: Rounds a floating point number downwards
+
+roundDown = math.floor(p)
+print(roundDown)
